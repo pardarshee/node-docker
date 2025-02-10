23c991e94c8e4dd2fe5dd21b08b6352eaf5af000
pardarshee <annkiparashar@gmail.com>
2025-02-10 08:59:56 +0530

    health check

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Docker!\n');
+  if (req.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('OK');
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, Docker!\n');
+  }
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
