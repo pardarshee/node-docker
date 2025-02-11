d25a32a379609b4a16d488d6ee967f4fcc444e76
pardarshee <annkiparashar@gmail.com>
2025-02-11 14:15:14 +0530

    fix

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, Pardarshee!");
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 6000; // Use Docker's assigned port if available
+
+// Main route
+app.get('/', (req, res) => {
+    res.send('Hi Pardarshee');
+});
+
+// Healthcheck route
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+app.listen(PORT, '0.0.0.0', () => {  // Listen on all network interfaces
+    console.log(`Server running on http://localhost:${PORT}`);
+});
