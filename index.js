4d3bbe523dd97ffeb48ca7496402913e05d997b1
pardarshee <annkiparashar@gmail.com>
2025-02-11 10:47:35 +0530

    upd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const SECRET_MESSAGE = process.env.MY_SECRET || "No env variable set";
 
 const server = http.createServer((req, res) => {
